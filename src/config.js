9b54f05a2525b9cdff264a301b70dd843c3fac99
MKondakova <milanon@ya.ru>
2021-05-01 12:09:37 +0300

    получены данные

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const WEBSOCKET_ADDRESS = 'ws://172.104.137.176:41239';
 export const TOKEN_LOCALSTORAGE_NAME = 'GoGameToken';
+export const API_ADDRESS = 'https://go-backend-denis.ambersoft.llc/';
--- src/views/Rating.vue
+++ src/views/Rating.vue
@@ -1,12 +1,53 @@
 <template>
-  <b-container fluid="lg" class="mt-3">
-    <h1>Рейтинг</h1>
-  </b-container>
+  <div>
+     <div v-if="loading" class="loading">
+      Loading...
+    </div>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+    <b-table v-if="leaderboard" striped hover :items="leaderboard"></b-table>
+  </div>
 </template>
 
 <script>
+import {TOKEN_LOCALSTORAGE_NAME, API_ADDRESS} from '../config.js'
+
+const REQUEST_PATH = 'leaderboard';
+
+
 export default {
-  name: "Settings",
+  name: "Rating",
   components: {},
+  data() {
+          return {
+            error: null, 
+            loading:true, 
+            leaderboard:null
+          };
+        },
+  created() {
+    localStorage.setItem(TOKEN_LOCALSTORAGE_NAME, 'de95db4f6bd32774ad32a4d56a85321cd1a0c573')
+    this.fetchData()
+    
+  },
+  methods: {
+    fetchData(){
+      this.error = this.liderboard = null
+      this.loading = true
+      fetch(API_ADDRESS+REQUEST_PATH +'?token=' + localStorage.getItem(TOKEN_LOCALSTORAGE_NAME))
+        .then((response)=>{
+          if (response.ok) {
+            return response.json();
+          } else {
+            this.error = 'Error!' + response.status+ ': ' +response.statusText;
+          }
+        })
+        .then((data = null)=> {
+          console.log(data?.leaderboard);
+          this.leaderboard = data?.leaderboard;
+        });
+    }
+  }
 };
 </script>
